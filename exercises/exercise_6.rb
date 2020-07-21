1d036cb32d34d0f821836d219b996e825a43fbfb
Jehanne Hodge <samarahjhodge@gmail.com>
2020-07-20 20:38:33 -0400

    implement exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Heather", last_name: "Turley", hourly_rate: 60)
+@store1.employees.create(first_name: "Jehanne", last_name: "Hodge", hourly_rate: 70)
+@store2.employees.create(first_name: "Jack", last_name: "Bielli", hourly_rate: 50)
+@store2.employees.create(first_name: "Katie", last_name: "Van Luven", hourly_rate: 100)
+
+
